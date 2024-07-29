c75257b7f8bbfa875162c8cad3dbf71c30dea443
Muhammad Alyan <alyan0332@gmail.com>
2024-07-29 22:56:01 +0500

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {}
+    colors: {
+      primaryText: "#FFF",
+      secondaryText: "#C1BBBB",
+      primaryBackground: "#010B13",
+      secondaryBackground: "#1E2121",
+      accent: "#FCE179",
+    },
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
