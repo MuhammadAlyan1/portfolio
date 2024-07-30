ecd9d0a9f6e4b53e978f930558e3b69cce612ed0
Muhammad Alyan <alyan0332@gmail.com>
2024-07-31 00:15:02 +0500

    Add tools and technologies list

---
+++ src/app/_components/technologies/toolsAndTechnologies.ts
@@ -0,0 +1,94 @@
+import HtmlIcon from "../../../assets/icons/technologies/html.svg";
+import CssIcon from "../../../assets/icons/technologies/css.svg";
+import SassIcon from "../../../assets/icons/technologies/sass.svg";
+import MantineIcon from "../../../assets/icons/technologies/mantine.svg";
+import MaterialIcon from "../../../assets/icons/technologies/materialUI.svg";
+import JavaScriptIcon from "../../../assets/icons/technologies/javascript.svg";
+import TypeScriptIcon from "../../../assets/icons/technologies/typescript.svg";
+import ReactIcon from "../../../assets/icons/technologies/react.svg";
+import NextIcon from "../../../assets/icons/technologies/nextjs.svg";
+import NodeIcon from "../../../assets/icons/technologies/node.svg";
+import MongoDBIcon from "../../../assets/icons/technologies/mongodb.svg";
+import ExpressIcon from "../../../assets/icons/technologies/express.svg";
+import GitIcon from "../../../assets/icons/technologies/git.svg";
+import TailwindIcon from "../../../assets/icons/technologies/tailwind.svg";
+import ReduxIcon from "../../../assets/icons/technologies/redux.svg";
+
+export const toolsAndTechnologies = [
+  {
+    icon: ReactIcon,
+    name: "React",
+    color: "#61DAFB",
+  },
+  {
+    icon: NextIcon,
+    name: "Next",
+    color: "#FFFFFF",
+  },
+  {
+    icon: NodeIcon,
+    name: "Node",
+    color: "#539E43",
+  },
+  {
+    icon: MongoDBIcon,
+    name: "MongoDB",
+    color: "#00684A",
+  },
+  {
+    icon: ExpressIcon,
+    name: "Express",
+    color: "#FFFFFF",
+  },
+  {
+    icon: HtmlIcon,
+    name: "HTML",
+    color: "#e34c26",
+  },
+  {
+    icon: CssIcon,
+    name: "CSS",
+    color: "#33A9DC",
+  },
+  {
+    icon: SassIcon,
+    name: "SCSS",
+    color: "#CD6799",
+  },
+  {
+    icon: MantineIcon,
+    name: "Mantine",
+    color: "#339AF0",
+  },
+  {
+    icon: MaterialIcon,
+    name: "Material UI",
+    color: "#007FFF",
+  },
+  {
+    icon: TailwindIcon,
+    name: "Tailwind",
+    color: "#38B2AC",
+  },
+
+  {
+    icon: TypeScriptIcon,
+    name: "TypeScript",
+    color: "#007ACC",
+  },
+  {
+    icon: JavaScriptIcon,
+    name: "JavaScript",
+    color: "#F7DF1E",
+  },
+  {
+    icon: ReduxIcon,
+    name: "Redux",
+    color: "#593D88",
+  },
+  {
+    icon: GitIcon,
+    name: "Git",
+    color: "#F34F29",
+  },
+];
