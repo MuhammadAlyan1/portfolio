137dfa925f70149c728deaac988c25ba3e9d01dc
Muhammad Alyan <alyan0332@gmail.com>
2024-07-30 18:36:48 +0500

    Add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "./_components/heroSection";
+
 export default function Home() {
   return (
-    <div className="border-2 bg-red-500 text-white">
-      <h1>Hello world</h1>
+    <div className="">
+      <HeroSection />
     </div>
   );
 }
