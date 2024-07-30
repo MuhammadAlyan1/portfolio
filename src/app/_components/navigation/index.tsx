7d018b6a2e269de586e2e9820757c6d1e8475dd3
Muhammad Alyan <alyan0332@gmail.com>
2024-07-30 21:59:13 +0500

    Add links

--- src/app/_components/navigation/index.tsx
+++ src/app/_components/navigation/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { FaUser } from "react-icons/fa";
 import { FaFolder } from "react-icons/fa";
@@ -8,10 +9,10 @@ import { MdEmail } from "react-icons/md";
 const Navigation = () => {
   return (
     <div className="fixed bottom-4 left-[50%] flex w-fit translate-x-[-50%] items-center gap-6 rounded-md bg-white/5 px-4 py-2 backdrop-blur-sm">
-      <IconText Icon={IoHomeSharp} label="Home" />
-      <IconText Icon={FaUser} label="About" iconSize={18} />
-      <IconText Icon={FaFolder} label="Projects" iconSize={19} />
-      <IconText Icon={MdEmail} label="Contact" />
+      <IconText Icon={IoHomeSharp} label="Home" id="landing" />
+      <IconText Icon={FaUser} label="About" iconSize={18} id="about-me" />
+      <IconText Icon={FaFolder} label="Projects" iconSize={19} id="projects" />
+      <IconText Icon={MdEmail} label="Contact" id="contact-me" />
     </div>
   );
 };
