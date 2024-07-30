e3d5aeeb6db82260af20eacb39a5600a94b977f4
Muhammad Alyan <alyan0332@gmail.com>
2024-07-30 21:59:07 +0500

    Add ability to navigate

--- src/app/_components/navigation/IconText.tsx
+++ src/app/_components/navigation/IconText.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link";
 import React from "react";
 import { IconType } from "react-icons";
 type IconTextProps = {
   Icon: IconType;
   label: string;
   iconSize?: number;
+  id: string;
 };
-const IconText: React.FC<IconTextProps> = ({ Icon, label, iconSize = 20 }) => {
+
+const IconText: React.FC<IconTextProps> = ({
+  Icon,
+  label,
+  iconSize = 20,
+  id,
+}) => {
   return (
-    <div className="group relative cursor-pointer">
+    <Link
+      className="group relative cursor-pointer"
+      href={`/#${id}`}
+      style={{ scrollBehavior: "smooth" }}
+    >
       <Icon
         size={iconSize}
         className="transition-all duration-200 ease-in-out group-hover:scale-125 group-hover:text-accent"
@@ -15,7 +27,7 @@ const IconText: React.FC<IconTextProps> = ({ Icon, label, iconSize = 20 }) => {
       <p className="absolute left-[50%] top-[-25px] translate-x-[-50%] scale-0 text-xs uppercase text-accent transition-transform duration-200 ease-in-out group-hover:scale-100">
         {label}
       </p>
-    </div>
+    </Link>
   );
 };
 
