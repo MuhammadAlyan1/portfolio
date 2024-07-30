de0b9528a1cdc1388f18e6092f2821c1ecfdc970
Muhammad Alyan <alyan0332@gmail.com>
2024-07-31 00:16:33 +0500

    Display tools and technologies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./_components/about";
 import HeroSection from "./_components/heroSection";
+import Technologies from "./_components/technologies";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
       <About />
+      <Technologies />
     </div>
   );
 }
