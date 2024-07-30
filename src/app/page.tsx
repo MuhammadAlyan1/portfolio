78d78d9b86ad9118d68700be24d7cd1e2347aba3
Muhammad Alyan <alyan0332@gmail.com>
2024-07-30 21:04:39 +0500

    Display about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./_components/about";
 import HeroSection from "./_components/heroSection";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
+      <About />
     </div>
   );
 }
