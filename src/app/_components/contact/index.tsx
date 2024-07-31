3ebe6385b8c2deb874e4815ee9c646dfac80f3c6
Muhammad Alyan <alyan0332@gmail.com>
2024-07-31 19:56:06 +0500

    Add contact firn

--- src/app/_components/contact/index.tsx
+++ src/app/_components/contact/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { IoMail, IoCall } from "react-icons/io5";
+import ContactForm from "./ContactForm";
 const Contact = () => {
   return (
     <section
@@ -12,10 +13,10 @@ const Contact = () => {
           Let's Talk
         </h1>
       </div>
-      <div className="mx-auto mt-16 flex w-fit flex-col items-start gap-4 lg:flex-row lg:gap-12">
+      <div className="mx-auto mt-16 flex w-full flex-col items-start gap-4 lg:flex-row lg:gap-12">
         <div className="mx-auto">
           <p className="mx-auto max-w-[15ch] text-center text-4xl md:text-5xl lg:text-start">
-            Ask me anything, or just say Hi.
+            Got questions? I'm here to help!
           </p>
           <div className="mx-auto my-4 flex w-fit flex-col gap-4 md:flex-row lg:mx-0 lg:mt-8 lg:flex-col">
             <a
@@ -34,49 +35,7 @@ const Contact = () => {
             </a>
           </div>
         </div>
-        <div className="w-[80vw] md:max-w-[650px]">
-          <form action="">
-            <div className="my-4 flex gap-2">
-              <label htmlFor="name" className="text-nowrap">
-                Hello, I'm
-              </label>
-              <input
-                type="text"
-                id="name"
-                placeholder="Muhammad Alyan"
-                required
-                className="w-full border-0 border-b-2 bg-primaryBackground px-1 text-accent outline-transparent focus-within:border-b-accent focus-within:outline-none focus:outline-transparent"
-              />
-            </div>
-            <div className="">
-              <label htmlFor="message">
-                And I'm looking for a skilled individual to
-              </label>
-              <textarea
-                rows={2}
-                id="message"
-                placeholder="Join my organization."
-                required
-                className="mt-4 block w-full border-0 border-b-2 bg-primaryBackground px-1 text-accent outline-transparent focus-within:border-b-accent focus-within:outline-none focus:outline-transparent"
-              />
-            </div>
-            <div className="mt-4 flex flex-wrap gap-2 sm:flex-nowrap">
-              <label htmlFor="email" className="text-nowrap">
-                You can reach me at
-              </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="alyan0332@gmail.com"
-                required
-                className="w-full border-0 border-b-2 bg-primaryBackground px-1 text-accent outline-transparent focus-within:border-b-accent focus-within:outline-none focus:outline-transparent"
-              />
-            </div>
-            <button className="my-4 w-full rounded-md border border-accent px-2 py-1 text-accent transition-colors duration-200 ease-in-out hover:bg-accent hover:text-primaryBackground">
-              Send Message
-            </button>
-          </form>
-        </div>
+        <ContactForm />
       </div>
     </section>
   );
